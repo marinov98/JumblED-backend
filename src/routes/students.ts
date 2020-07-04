471fbc8cf1069544b33eeaedd6eb7c2727e8d393
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-07-03 22:20:17 -0400

    extra student routes

--- src/routes/students.ts
+++ src/routes/students.ts
@@ -1,8 +1,34 @@
 import express from "express";
 import passport from "passport";
-import { Student, Test } from "./../db/models";
+import IUser from "./../db/interfaces/user";
+import { Test, Class } from "./../db/models";
 
 const router = express.Router();
+/**
+ *  Get classes
+ *  @route PATCH api/students/classes
+ *  @desc grab all the classes
+ *  @access Protected
+ */
+router.patch(
+  "/classes",
+  passport.authenticate("jwt", { session: false }),
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const classes: Array<any> = await Class.find().populate("teacher");
+
+      if (!classes) return res.status(404).json({ error: "No classes found!" });
+
+      return res.status(201).json(classes);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 /**
  *  Get test
@@ -10,7 +36,7 @@ const router = express.Router();
  *  @desc grab the test with questions
  *  @access Protected
  */
-router.get(
+router.patch(
   "/tests/:code",
   passport.authenticate("jwt", { session: false }),
   async (
@@ -26,6 +52,33 @@ router.get(
       if (!testToTake) return res.status(404).json({ error: "Test not found" });
 
       return res.status(201).json(testToTake);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+/**
+ *  Register for a class
+ *  @route PATCH api/students/classes
+ *  @desc grab the test with questions
+ *  @access Protected
+ */
+router.patch(
+  "/classes/:classid",
+  passport.authenticate("jwt", { session: false }),
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const classToUpdate = await Class.findById(req.params.classid);
+      if (!classToUpdate)
+        return res.status(404).json({ error: "Class not found" });
+
+      const user = req.user as IUser;
+      classToUpdate.students.push(user._id);
     } catch (err) {
       next(err);
     }
