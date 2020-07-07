5e85572c77d03c95cba08b9498beed4f32ed1698
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-07-07 16:08:03 -0400

    removed google passport and useless env vars

--- src/utils/config/keys.ts
+++ src/utils/config/keys.ts
@@ -10,8 +10,5 @@ const port: number = parseInt(process.env.PORT!) || 3002;
 const dbUrl: string =
   process.env.DB_URL || "mongodb://localhost:27017/jumbledTest";
 const jwtSecret: string = process.env.SECRET || "VERYSUPERSECRETLYSECRET";
-const refreshSecret: string = process.env.REFRESH_SECRET || "REFRESHingSecret";
-const googleSecret: string = process.env.GOOGLE_SECRET || "googleSecret";
-const googleClientId: string = process.env.GOOGLE_CLIENT_ID || "googleClientId";
 
-export { port, dbUrl, jwtSecret, refreshSecret, googleSecret, googleClientId };
+export { port, dbUrl, jwtSecret };
--- src/utils/config/passport.ts
+++ src/utils/config/passport.ts
@@ -1,8 +1,7 @@
 import passport from "passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { OAuth2Strategy } from "passport-google-oauth";
 import { Teacher, Student } from "../../db/models";
-import { jwtSecret, googleSecret, googleClientId } from "./keys";
+import { jwtSecret } from "./keys";
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -23,39 +22,6 @@ passport.use(
 
           if (!student) done(null, false);
           else done(null, student);
-        }
-      } catch (err) {
-        done(err);
-      }
-    }
-  )
-);
-
-const googleOptions = {
-  clientID: googleClientId,
-  clientSecret: googleSecret,
-  callbackURL: "http://www.example.com/auth/google/callback"
-};
-
-passport.use(
-  new OAuth2Strategy(
-    googleOptions,
-    async (
-      accessToken: any,
-      refreshToken: any,
-      profile: any,
-      done: Function
-    ): Promise<any> => {
-      try {
-        const teacher = await Teacher.findOne({ googleId: profile.id });
-
-        if (teacher) {
-          return done(null, teacher);
-        } else {
-          const student = await Student.findOne({ googleId: profile.id });
-
-          if (!student) return done(null, false);
-          else return done(null, student);
         }
       } catch (err) {
         done(err);
